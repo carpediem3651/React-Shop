19d87158bc3f906684f6f4ff61a5019a6accdcaa
Nam Ki Hyeok <carpediem7490@gmail.com>
2022-12-06 09:52:24 +0900

    이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Navbar,Nav,Container} from 'react-bootstrap';
+import {Navbar,Nav,Container,Col,Row} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
        <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+      <Container>
+        <Row>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }

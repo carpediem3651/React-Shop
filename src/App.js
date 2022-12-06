428990d1902bf7ae2e13b98b556b0581357b2ce9
Nam Ki Hyeok <carpediem7490@gmail.com>
2022-12-06 12:02:10 +0900

    제품등록, props, map 활용

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Nav,Container,Col,Row} from 'react-bootstrap';
+import data from './data.js';
+import {useState} from "react";
 
 function App() {
+
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
        <Navbar bg="dark" variant="dark">
@@ -19,25 +24,30 @@ function App() {
       <div className="main-bg"></div>
       <Container>
         <Row>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
-          <Col>
-            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-          </Col>
+          {/* <Card shoes={shoes[0]} i={1}></Card>
+          <Card shoes={shoes[1]} i={2}></Card>
+          <Card shoes={shoes[2]} i={3}></Card> */}
+          {
+            shoes.map((a, i) => {
+              return (
+                <Card shoes={shoes[i]} i={i}></Card>
+              )
+            })
+          }
         </Row>
       </Container>
     </div>
   );
+}
+
+function Card(props) {
+  return (
+    <Col>
+      <img src={'https://codingapple1.github.io/shop/shoes'+(props.i+1)+'.jpg'} width="80%"/>
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </Col>
+  )
 }
 
 export default App;

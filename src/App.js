1e40b3232e49996414dfbb8de370890311b92474
Nam Ki Hyeok <carpediem7490@gmail.com>
2022-12-07 11:24:14 +0900

    리액트 라우터 셋팅, navigate,nested routes, outlet 적용

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Nav,Container,Col,Row} from 'react-bootstrap';
 import data from './data.js';
 import {useState} from "react";
+import {Routes, Route, Link, useNavigate, Outlet} from 'react-router-dom'
+import Detail from './routes/Detail.js'
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -15,27 +18,41 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/detail')}}>Detail</Nav.Link>
+            <Nav.Link onClick={()=>{navigate('/about')}}>about</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      
+      <Routes>
+        <Route path='/' element={      
+        <>
+        <div className="main-bg"></div>
+        <Container>
+          <Row>
+            {
+              shoes.map((a, i) => {
+                return (
+                  <Card shoes={shoes[i]} i={i}></Card>
+                )
+              })
+            }
+          </Row>
+        </Container>
+        </>
+        } />
+        <Route path="/detail"  element={<Detail/>}/>
 
-      <div className="main-bg"></div>
-      <Container>
-        <Row>
-          {/* <Card shoes={shoes[0]} i={1}></Card>
-          <Card shoes={shoes[1]} i={2}></Card>
-          <Card shoes={shoes[2]} i={3}></Card> */}
-          {
-            shoes.map((a, i) => {
-              return (
-                <Card shoes={shoes[i]} i={i}></Card>
-              )
-            })
-          }
-        </Row>
-      </Container>
+        <Route path="/about"  element={<About/>}>
+          <Route path="member" element={<div>멤버</div>} />
+          <Route path="location" element={<div>위치정보</div>} />
+        </Route>
+
+        <Route path="*" element={<div>없는페이지입니다.</div>} />
+      </Routes>
+
+
     </div>
   );
 }
@@ -49,5 +66,15 @@ function Card(props) {
     </Col>
   )
 }
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
 
 export default App;
